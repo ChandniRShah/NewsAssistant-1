15ab1613fa371d6c86dcbf3e1925ca86393ca9c8
Pradeep Prakash <36431711+pradeep-prakash@users.noreply.github.com>
2018-05-08 22:20:06 -0400

    Add files via upload

---
+++ newschatbot/accounts/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+import django
+from . import views
+from django.contrib.auth.views import login, logout
+from django import forms
+
+urlpatterns = [
+     
+     url(r'^login/$', login, {'template_name': 'accounts/login.html'}),
+     url(r'^logout/$', logout, {'template_name': 'accounts/logout.html'}),
+     url(r'^register/$', views.register, name='register'),
+   
+    ]
---
+++ newschatbot/home/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import TemplateView
+
+class HomeView(TemplateView):
+    template_name = 'home/home2.html'
+
+
+
+
+
+class ChatterBotAppView(TemplateView):
+    template_name = 'home/home.html'
+    
+
